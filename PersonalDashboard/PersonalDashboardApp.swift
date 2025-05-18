3b4ffd452bdeb5c1ef66d7700bf9bef9fe39b968
Hana Harris <Selkiefawn@gmail.com>
2025-05-18 12:41:22 -0400

    Initial Commit

---
+++ PersonalDashboard/PersonalDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalDashboardApp.swift
+//  PersonalDashboard
+//
+//  Created by Morgan Harris on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
